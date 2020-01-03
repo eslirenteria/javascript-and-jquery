6ddca88cde7a167c9e3302435a843a659a50856c
Jossue Renteria <jrenteria@unitedvirtualities.com>
2020-01-03 10:54:05 -0700

    last commit

--- nodejs-getting-started/ngs/7-web/1-hello-world.js
+++ nodejs-getting-started/ngs/7-web/1-hello-world.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
-});
+const requestListener = (req, res) => {
+  console.dir(res, { depth:0 });
+  // req and res are streams.
+  // console.log(req.url);
+  res.write('Hello!\n');
+  res.end();
+};
+
+const server = http.createServer();
+server.on('request', requestListener);
 
 server.listen(4242, () => {
   console.log('Server is running...');
